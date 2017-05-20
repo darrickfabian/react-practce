4aa083b314d7147c86463d12adad8feb7572b8da
darrick fabian <dfabian@solutionset.com>
2017-05-20 16:17:08 -0700

    update app with selected navigation component

--- app/index.js
+++ app/index.js
@@ -1,17 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var PropTypes = require('prop-types');
+var App = require('./components/App');
 require('./index.css');
 
 
-class App extends React.Component {
-  render () {
-    return (
-      <div>
-        Hello React Training!
-      </div>
-    )
-  }
-}
+
 
 
 ReactDOM.render(
